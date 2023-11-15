65acb3f0ec90bc3c6fcb7f9dc1400b60b7377c5a
MAUMAU101 <40014287+MAUMAU101@users.noreply.github.com>
2023-11-14 21:28:55 -0800

    updated screens

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -3,8 +3,7 @@ export 'package:triki_triki2/screens/buttons/ButtonsScreen .dart';
 export 'package:triki_triki2/screens/screens.dart';
 export 'package:triki_triki2/screens/ui_controls/ui_controls_screen.dart';
 export 'package:triki_triki2/screens/snackbar/snackbar_screen.dart';
-
 export 'package:triki_triki2/screens/progress/progress_screen.dart';
-
 export 'package:triki_triki2/screens/home/home_screen.dart';
-
+export 'package:triki_triki2/screens/infinate_scroll/infinate_scroll_screen.dart';
+export 'package:triki_triki2/screens/animated/animated_screen.dart';
